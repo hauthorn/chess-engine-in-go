62d4ab4b5887b4b6ac87c30b42be892f7b4b8643
Christoffer Hauthorn <christoffer@emplate.it>
2022-08-06 11:50:32 +0200

    Added benchmarks for AlphaBetaEngine - it crushes the NaiveEngine already at depth 4

--- engines/engines-benchmark_test.go
+++ engines/engines-benchmark_test.go
@@ -30,3 +30,37 @@ func BenchmarkNaiveEngine_BestMoveDepth4(b *testing.B) {
 		naive.BestMove(g)
 	}
 }
+
+// AlphaBetaEngine benchmarks
+
+func BenchmarkAlphaBetaEngine_BestMoveDepth2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		g := chess.NewGame()
+		naive := AlphaBetaEngine{Depth: 2}
+		naive.BestMove(g)
+	}
+}
+
+func BenchmarkAlphaBetaEngine_BestMoveDepth3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		g := chess.NewGame()
+		naive := AlphaBetaEngine{Depth: 3}
+		naive.BestMove(g)
+	}
+}
+
+func BenchmarkAlphaBetaEngine_BestMoveDepth4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		g := chess.NewGame()
+		naive := AlphaBetaEngine{Depth: 4}
+		naive.BestMove(g)
+	}
+}
+
+func BenchmarkAlphaBetaEngine_BestMoveDepth5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		g := chess.NewGame()
+		naive := AlphaBetaEngine{Depth: 5}
+		naive.BestMove(g)
+	}
+}
